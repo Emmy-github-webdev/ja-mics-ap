cb21ac466e305a6b32fcaab760e6778d5a073687
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-19 11:08:25 +0200

    Updated the service endpoint

--- payment-service/src/main/java/com/ecommerce/paymentservice/controller/PaymentController.java
+++ payment-service/src/main/java/com/ecommerce/paymentservice/controller/PaymentController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/payments")
+@RequestMapping("/payments")
 public class PaymentController {
 
     private final PaymentService paymentService;
